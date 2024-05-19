be317a45f6f6a41c76f252551f87e21597689c95
Hugo Persson <hugo.e.persson@gmail.com>
2024-05-19 22:22:59 +0200

    Added Formula

---
+++ Formula/dns-cli-tools.rb
@@ -0,0 +1,25 @@
+class DnsClDnsCli < Formula
+  desc "A CLI tool to manage dns entries"
+  homepage "https://github.com/Hugo-Persson/dns-cli-tools"
+  version "1.0.5"
+  # Define the URL and SHA256 for macOS
+  on_macos do
+    # TODO: Support for intel mac
+    url "https://github.com/Hugo-Persson/dns-cli-tools/releases/download/#{version}/dns-cli-macos-arm64.tar.gz"
+    sha256 "8a071d3833347fecda9a9afc06bd880d8bfef28b3f960a5027538ecf128b3d7a"
+  end
+
+  # Define the URL and SHA256 for Linux
+  on_linux do
+    url "https://github.com/Hugo-Persson/dns-cli-tools/releases/download/#{version}/dns-cli-linux-amd64.tar.gz"
+    sha256 "349281dd23ad3a086be98064c9b42c78aea2285fcc1446513ed6b97863423698"
+  end
+
+  def install
+    bin.install "dns-cli"
+  end
+
+  test do
+    system "#{bin}/dns-cli"
+  end
+end

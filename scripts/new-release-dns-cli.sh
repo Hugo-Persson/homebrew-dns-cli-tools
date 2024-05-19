16cdb67a8e8b4707d3ffcfb2a9f2799f074979e5
Hugo Persson <hugo.e.persson@gmail.com>
2024-05-19 23:14:00 +0200

    Added release script

---
+++ scripts/new-release-dns-cli.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+VERSION=$1
+echo "Updating to version $VERSION"
+
+# cd to script dir
+
+# Get the directory path of the script being run
+script_dir=$(dirname "$(readlink -f "$0")")
+
+# Change the working directory to the script's directory
+cd "$script_dir"
+cd ..
+cd Formula
+
+sed -i '' "s/version \"[0-9]\.[0-9]\.[0-9]\"/version \"$VERSION\"/" ./dns-cli-tools.rb
+
+BASE_URL="https://github.com/Hugo-Persson/dns-cli-tools/releases/download/$VERSION"
+
+MAC_NAME="dns-cli-macos-arm64.tar.gz"
+LINUX_NAME="dns-cli-linux-amd64.tar.gz"
+
+wget -q "$BASE_URL/$MAC_NAME"
+MAC_SHA=$(shasum -a 256 "$MAC_NAME" | awk '{ print $1 }')
+sed -i '' "s/mac_sha ".*"/mac_sha \"$MAC_SHA\"/" ./dns-cli-tools.rb
+rm "$MAC_NAME"
+
+wget -q "$BASE_URL/$LINUX_NAME" >/dev/null
+LINUX_SHA=$(shasum -a 256 "$LINUX_NAME" | awk '{ print $1 }')
+sed -i '' "s/linux_sha ".*"/linux_sha \"$LINUX_SHA\"/" ./dns-cli-tools.rb
+rm "$LINUX_NAME"

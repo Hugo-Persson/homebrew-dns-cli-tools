8d948dd46878cac4ac3cda7cbe2935f4b4c65e21
GitHub Action <action@github.com>
2024-05-20 21:00:30 +0000

    Automated script run

--- Formula/dns-cli-tools.rb
+++ Formula/dns-cli-tools.rb
@@ -1,10 +1,10 @@
 class DnsCliTools < Formula
   desc "A CLI tool to manage dns entries"
   homepage "https://github.com/Hugo-Persson/dns-cli-tools"
-  version "1.0.7"
+  version "1.0.9"
   @github_base_url = "https://github.com/Hugo-Persson/dns-cli-tools/releases/download/#{version}"
-  @mac_sha = "1b8c4d8854b4bf586793e2cf615d2705bf31ce624ae0d0dbb42f27be569f605b"
-  @linux_sha = "0932c974baf3bb89da383ca65c139ab66736325e3430bcf7d81fb838e68ebb6b"
+  @mac_sha = "4d01c0837645807329cd4b1798f5d4b98c2e79f07cc6a5c296254dff6dd37ecb"
+  @linux_sha = "b8b037fb4fcf9c6e3c5733c4ccf8973ec86e03e45e6e3a8f626d92ad1ba7e23a"
   on_macos do
     url "#{@github_base_url}dns-cli-macos-arm64.tar.gz"
     sha256 @mac_sha

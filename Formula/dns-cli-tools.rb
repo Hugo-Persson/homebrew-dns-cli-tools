b57ba0312299a84601b1018103d0d395788d316c
Hugo Persson <hugo.e.persson@gmail.com>
2024-08-06 22:18:13 +0200

    Fix to version

--- Formula/dns-cli-tools.rb
+++ Formula/dns-cli-tools.rb
@@ -1,7 +1,7 @@
 class DnsCliTools < Formula
   desc "A CLI tool to manage dns entries"
   homepage "https://github.com/Hugo-Persson/dns-cli-tools"
-  version "1.0.19"
+  version "1.0.18"
   @github_base_url = "https://github.com/Hugo-Persson/dns-cli-tools/releases/download/#{version}"
   @mac_sha = "b3e2bca4b4f410b2333109994f50fcb56b9f936867ed2e4846848c14c104b1c3"
   @linux_sha = "be50099b3ebb789c7c0d99641a4e9880fa4e99130042430db28953e31dd96409"
